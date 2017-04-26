0ca0141d961114557c470873482ec381c5be0aad
Thomas Beer <t.beer90@gmail.com>
2017-04-26 23:25:15 +0200

    Test

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// write your code here
 
         System.out.println("Teste Git");
-        System.out.println("2.Test");
-        System.out.println("3.Test");
+        System.out.println("Teste Git 22");
+
     }
 }
